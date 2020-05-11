47bf3f63bfa92610249523938264b80f0339e793
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-05-11 21:06:53 +0200

    sw register on root domain

--- app/init.ts
+++ app/init.ts
@@ -14,7 +14,7 @@ export async function init() {
     if (navigator.serviceWorker.controller) {
       console.log("[SW] Found Service worker")
     } else {
-      await navigator.serviceWorker.register("./sw.js", {scope: "./"}).then(function(reg){
+      await navigator.serviceWorker.register("/sw.js", {scope: "/"}).then(function(reg){
         console.log("[SW] Service worker installed with scope: " + reg.scope)
       })
     }

d383c4c883c6d0f8a883f129865cecc077f5f3cf
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-05-18 17:58:11 +0200

    basic layout for cards and background image

--- app/_component/_frame/_pageSection/landingSection/landingSection.ts
+++ app/_component/_frame/_pageSection/landingSection/landingSection.ts
@@ -2,15 +2,24 @@ import { declareComponent } from "../../../../lib/declareComponent"
 import PageSection from "../pageSection"
 import "./../../../_themeAble/_text/textblob/textblob"
 import TextBlob from "./../../../_themeAble/_text/textblob/textblob"
+import IconCard from "../../../_themeAble/_card/iconCard/iconCard";
+import BioMedIcon from "../../../_themeAble/_icon/bioMed/bioMed";
 
-let te = new TextBlob()
 
 
 export default declareComponent("landing-section", class Landing extends PageSection {
-  
+  private cardContainer = this.q("card-container")
   constructor() {
     super()
     
+    this.cardContainer.apd(
+      new IconCard(new BioMedIcon, "Biomedizin"), 
+      new IconCard(new BioMedIcon, "Biomedizin"), 
+      new IconCard(new BioMedIcon, "Biomedizin"), 
+      new IconCard(new BioMedIcon, "Biomedizin"), 
+      new IconCard(new BioMedIcon, "Biomedizin"), 
+      new IconCard(new BioMedIcon, "Biomedizin"), 
+      new IconCard(new BioMedIcon, "Biomedizin"))
   }
 
   protected activationCallback(active: boolean) {
--- app/_component/_themeAble/_card/iconCard/iconCard.ts
+++ app/_component/_themeAble/_card/iconCard/iconCard.ts
@@ -1,13 +1,43 @@
 import Card from "../card"
 import declareComponent from "../../../../lib/declareComponent"
+import Icon from "./../../_icon/icon"
+import { Data } from "josm"
 
 
 
 
 export default class IconCard extends Card {
-  constructor() {
-    super()      
+  private iconContainer = this.q("icon-container")
+  private contentContainer = this.q("content-container")
+  constructor(icon: Icon, content: string | Data<string>) {
+    super()
+    this.icon(icon)
+    this.content(content)
+
   }
+
+  content(): string
+  content(to: string | Data<string>): void
+  content(to?: string | Data<string>): any {
+    this.contentContainer.text(to)
+  }
+
+  icon(): Icon
+  icon(to: Icon): Promise<void>
+  icon(to?: Icon): any {
+    if (to) {
+      return (async () => {
+        let first = this.contentContainer.childs(1, true).empty
+        if (!first) await this.contentContainer.anim({opacity: 0})
+        this.contentContainer.removeChilds()
+        this.contentContainer.apd(to)
+        if (!first) await this.contentContainer.anim({opacity: 1})
+      })()
+      
+    }
+    else this.contentContainer.childs()
+  }
+
 
   stl() {
     return super.stl() + require("./iconCard.css").toString()

7e5b689f118a8d2a0977a0c6c815422121fe0886
Georg Felber <gfelber@student.tgm.ac.at>
2020-05-20 10:37:17 +0200

    added anmelden card base files

---
+++ app/_component/_themeAble/_card/anmeldenCard/anmeldenCard.ts
@@ -0,0 +1,29 @@
+import declareComponent from "../../../../lib/declareComponent";
+import Card from "../card";
+import "../../link/link"
+import {Theme} from "../../themeAble";
+import Textblob from "../../_text/textblob/textblob";
+
+export default declareComponent("anmelden-card", class AnmeldenCard extends Card {
+
+    private textblob = this.q("c-textblob") as Textblob;
+
+    constructor(){
+        super(false);
+    }
+
+    theme():Theme
+    theme(to:Theme):void
+    theme(to?:Theme):any{
+        this.textblob.theme(to);
+        return super.theme(to);
+    }
+
+    stl(){
+        return super.stl() + require('./anmeldenCard.css').toString();
+    }
+
+    pug(){
+        return require('./anmeldenCard.pug').default;
+    }
+})

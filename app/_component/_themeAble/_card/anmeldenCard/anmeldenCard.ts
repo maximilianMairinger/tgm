b16a02aeaca59ece2eaa6729d0f005c95361a55c
Georg Felber <gfelber@student.tgm.ac.at>
2020-06-02 11:43:12 +0200

    added zertificateMan

--- app/_component/_themeAble/_card/anmeldenCard/anmeldenCard.ts
+++ app/_component/_themeAble/_card/anmeldenCard/anmeldenCard.ts
@@ -3,10 +3,13 @@ import Card from "../card";
 import "../../link/link"
 import {Theme} from "../../themeAble";
 import Textblob from "../../_text/textblob/textblob";
+import "../../../_themeAble/_icon/zertificateMan/zertificateMan"
+import ZertificateMan from "../../_icon/zertificateMan/zertificateMan"
 
 export default declareComponent("anmelden-card", class AnmeldenCard extends Card {
 
     private textblob = this.q("c-textblob") as Textblob;
+    private zertificateMan = this.q("c-zertificate-man") as ZertificateMan;
 
     constructor(){
         super(false);
@@ -15,6 +18,7 @@ export default declareComponent("anmelden-card", class AnmeldenCard extends Card
     theme():Theme
     theme(to:Theme):void
     theme(to?:Theme):any{
+        this.zertificateMan.theme(to);
         this.textblob.theme(to);
         return super.theme(to);
     }

5fec7be0ac549808891cc4242431f89448bd0b86
Georg Felber <gfelber@student.tgm.ac.at>
2020-06-03 17:29:38 +0200

    added selection method for adding selectables

--- app/_component/_themeAble/_card/selectionCard/selectionCard.ts
+++ app/_component/_themeAble/_card/selectionCard/selectionCard.ts
@@ -1,13 +1,21 @@
 import declareComponent from "../../../../lib/declareComponent";
 import Card from "../card";
 import "../../link/link"
+import Link from "../../link/link"
 import {Theme} from "../../themeAble";
 import Textblob from "../../_text/textblob/textblob";
 import "../../../_button/button"
+import Button from "../../../_button/button"
+import "../../../_themeAble/_icon/tie/tie"
+import "../../../_themeAble/_icon/cpu/cpu"
+import "../../../_themeAble/_icon/tools/tools"
+import "../../../_themeAble/_icon/windmill/windmill"
 
+type SelectionOptions = {icon: string, title: string, content: string, link: string}[]
 export default declareComponent("selection-card", class SelectionCard extends Card {
 
     private textblob = this.q("c-textblob") as Textblob;
+    private _options:SelectionOptions;
 
     constructor(){
         super(false);
@@ -18,6 +26,38 @@ export default declareComponent("selection-card", class SelectionCard extends Ca
     theme(to?:Theme):any{
         this.textblob.theme(to);
         return super.theme(to);
+    }
+
+    selection():SelectionOptions
+    selection(options:SelectionOptions | JSON):void
+    selection(options?:SelectionOptions | JSON){
+        if(options){
+            this._options = this.parseJSONProp(options);
+            console.log(this._options);
+            let container = this.q("selection-container");
+            for(let i = 0; i < this._options.length; i++){
+                let box = ce("selection-box");
+                let content = ce("selection-content");
+                let icon = ce("icon-container");
+                icon.append(ce("c-" + this._options[i].icon +"-icon"));
+                let infocontainer = ce("info-container");
+                let link = ce("link-container");
+                link.append(new Link("Mehr", this._options[i].link));
+                let textcontainer = ce("text-container");
+                textcontainer.append(ce("heading-text").text(this._options[i].title));
+                textcontainer.append(ce("content-text").text(this._options[i].content));
+                infocontainer.append(link);
+                infocontainer.append(textcontainer);
+                content.append(icon);
+                content.append(infocontainer);
+                let button = new Button();
+                button.link(this._options[i].link);
+                box.append(ce("selection-background"));
+                box.append(content);
+                box.append(button);
+                container.append(box);
+            }
+        }else return this._options
     }
 
     stl(){

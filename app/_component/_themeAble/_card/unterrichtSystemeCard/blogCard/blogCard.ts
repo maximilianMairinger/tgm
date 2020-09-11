3b202c7ca730cf530ee19bf3422a2bd11b6e6f82
Georg Felber <gfelber@student.tgm.ac.at>
2020-09-11 22:38:37 +0200

    now allows Date as note parameter

--- app/_component/_themeAble/_card/unterrichtSystemeCard/blogCard/blogCard.ts
+++ app/_component/_themeAble/_card/unterrichtSystemeCard/blogCard/blogCard.ts
@@ -1,8 +1,6 @@
 import declareComponent from "../../../../../lib/declareComponent";
 import ThemeAble, {Theme} from "../../../themeAble";
 import UnterrichtSystemeCard from "../unterrichtSystemeCard";
-import Textblob from "../../../_text/textblob/textblob";
-import Card from "../../card";
 
 export default class BlogCard extends UnterrichtSystemeCard {
 
@@ -10,6 +8,17 @@ export default class BlogCard extends UnterrichtSystemeCard {
         super();
         this.textblob.hsize({max:25, min:25})
         this.textblob.hmobile({max:25, min:25})
+    }
+
+    note():string
+    note(note:Date|string):void
+    note(note?:Date|string){
+        console.log(new Date(note).toDateString() );
+        if(new Date(note).toDateString() !== 'Invalid Date'){
+            let moment = require("moment");
+            this.q("note-text").text(moment(note).format("DD.MM.YY"));
+        } else if (typeof note === 'string') super.note(note as string);
+        else return this.q("note-text").text();
     }
 
     stl(){

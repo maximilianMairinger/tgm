1f649f5badf9ceb123e0b415761a48691732df91
Georg Felber <gfelber@student.tgm.ac.at>
2020-09-13 15:45:23 +0200

    blog-card is now clickable

--- app/_component/_themeAble/_card/_infoCard/blogCard/blogCard.ts
+++ app/_component/_themeAble/_card/_infoCard/blogCard/blogCard.ts
@@ -1,13 +1,16 @@
 import declareComponent from "../../../../../lib/declareComponent";
 import InfoCard from "../infoCard";
-import Textblob from "../../../_text/textblob/textblob";
+import Button from "../../../../_button/button";
 
 export default class BlogCard extends InfoCard {
 
+    private button = new Button();
+
     constructor() {
         super();
-        this.textblob.hsize({max:25, min:25})
-        this.textblob.hmobile({max:25, min:25})
+        this.textblob.hsize({max:25, min:25});
+        this.textblob.hmobile({max:25, min:25});
+        this.q("info-card").append(this.button);
     }
 
     note():string
@@ -19,6 +22,11 @@ export default class BlogCard extends InfoCard {
             this.q("note-text").text(moment(note).format("DD.MM.YY"));
         } else if (typeof note === 'string') super.note(note as string);
         else return this.q("note-text").text();
+    }
+
+    href(href?:string):any{
+        this.button.link(href);
+        return super.href(href);
     }
 
     stl(){

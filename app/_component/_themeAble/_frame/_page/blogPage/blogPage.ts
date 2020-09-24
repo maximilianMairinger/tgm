ce7845f8e7f2c64a471b62afd5196d5050c303b6
Georg Felber <gfelber@student.tgm.ac.at>
2020-09-24 16:49:14 +0200

    integrated blog suggestions

--- app/_component/_themeAble/_frame/_page/blogPage/blogPage.ts
+++ app/_component/_themeAble/_frame/_page/blogPage/blogPage.ts
@@ -2,10 +2,13 @@ import { declareComponent } from "../../../../../lib/declareComponent"
 import Page from "../page"
 import * as domain from "./../../../../../lib/domain"
 import Blog from "../../../../_themeAble/_text/blogPost/blogPost"
+import BlogSuggestions, {blogCardInfo} from "../../../blogSuggestions/blogSuggestions";
 import GhostContentAPI from '@tryghost/content-api'
+import BlogCard from "../../../_card/_infoCard/blogCard/blogCard";
 
 
 // change after deployment to root url
+//@ts-ignore
 const api = new GhostContentAPI({
   url: ' https://dev.tgmrebrand.xyz',
   key: '062f128c326e0312972d41f705',
@@ -37,13 +40,34 @@ export default class BlogPage extends Page {
       let blog = new Blog();
       console.log(blogData.title);
       blog.blogtitle(blogData.title);
-      blog.date(blogData['published_at']);
-      blog.image(blogData['feature_image']);
+      blog.date(blogData.published_at);
+      blog.image(blogData.feature_image);
       blog.htmlcontent(blogData.html);
+      blog.css({"order": 1});
       this.elementBody.append(blog);
     }).catch((err) => {
       console.error(err);
+      let error = ce("error-message");
+      error.text("404 Not Found");
+      this.elementBody.append(error);
     });
+    api.posts.browse({limit:5}).then((blogData) => {
+      let suggestions = new BlogSuggestions();
+      suggestions.blogs(blogData.map((blog) => {
+        let blogCard:blogCardInfo = {
+          heading: blog.title,
+          date: blog.published_at,
+          content: blog.excerpt,
+          thumbnail: blog.feature_image,
+          link: blog.url
+        }
+        return blogCard;
+      }));
+      suggestions.css({"order": 2});
+      this.elementBody.append(suggestions)
+    }).catch((err) => {
+      console.error(err);
+    })
     this.blogLoaded = true;
   }
 

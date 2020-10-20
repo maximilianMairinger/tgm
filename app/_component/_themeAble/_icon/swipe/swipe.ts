aefc7e6c8c9a538fa29b866226e218abafbdc2e2
Georg Felber <gfelber@student.tgm.ac.at>
2020-10-20 10:29:44 +0200

    made swipe-icon themeable

--- app/_component/_themeAble/_icon/swipe/swipe.ts
+++ app/_component/_themeAble/_icon/swipe/swipe.ts
@@ -1,5 +1,6 @@
 import Icon from "../icon";
 import declareComponent from "../../../../lib/declareComponent";
+import {Theme} from "../../themeAble";
 
 
 export default declareComponent("swipe-icon", class SwipeAnimation extends Icon {
@@ -7,7 +8,21 @@ export default declareComponent("swipe-icon", class SwipeAnimation extends Icon 
         super();
     }
 
+
+    theme(): Theme
+    theme(to: Theme): this
+    theme(to?: Theme): any {
+        if(to == "dark"){
+            this.q("object").css({"filter": "brightness(0) invert(1)"});
+        }
+        return super.theme(to)
+    }
+
     pug() {
         return require("./swipe.pug").default
+    }
+
+    stl(){
+        return super.stl() + require('./swipe.css').toString();
     }
 })

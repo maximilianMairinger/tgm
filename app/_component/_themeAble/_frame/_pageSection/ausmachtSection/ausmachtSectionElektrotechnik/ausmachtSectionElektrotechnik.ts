166a4a45d1425a31eba83ec3a3fbfda6317891ed
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-12-10 00:16:38 +0100

    links ausmacht section het

--- app/_component/_themeAble/_frame/_pageSection/ausmachtSection/ausmachtSectionElektrotechnik/ausmachtSectionElektrotechnik.ts
+++ app/_component/_themeAble/_frame/_pageSection/ausmachtSection/ausmachtSectionElektrotechnik/ausmachtSectionElektrotechnik.ts
@@ -17,11 +17,11 @@ export default declareComponent("ausmacht-section-elektrotechnik", class extends
                 heading: "Vertiefungen",
                 note: "Fachspezifische",
                 selection: [
-                    {"icon":"energysysteme", "title":"Energiesysteme", "content":"Energiegewinnung und -speicherung", "link":"/"},
-                    {"icon":"automatisierungstechnik", "title":"Automatisierungstechnik", "content":"Automatisieren in elektrotechnischen Anwendungen", "link":"/"},
-                    {"icon":"antriebstechnik", "title":"Antriebstechnik", "content":"Bewegung und Elektrotechnik", "link":"/"},
-                    {"icon":"industrieelektronik", "title":"Industrieelektronik", "content":"Elektrotechnik in der Industrie", "link":"/"},
-                    {"icon":"angewandteInformatik", "title":"Angewandte Informatik", "content":"Entwickeln von computerbasierten Lösungen", "link":"/"}
+                    {"icon":"energysysteme", "title":"Energiesysteme", "content":"Energiegewinnung und -speicherung", "link":"tagesschule/elektrotechnik/highlights/energiesysteme"},
+                    {"icon":"automatisierungstechnik", "title":"Automatisierungstechnik", "content":"Automatisieren in elektrotechnischen Anwendungen", "link":"tagesschule/elektrotechnik/highlights/automatisierungstechnik"},
+                    {"icon":"antriebstechnik", "title":"Antriebstechnik", "content":"Bewegung und Elektrotechnik", "link":"tagesschule/elektrotechnik/highlights/antriebstechnik"},
+                    {"icon":"industrieelektronik", "title":"Industrieelektronik", "content":"Elektrotechnik in der Industrie", "link":"tagesschule/elektrotechnik/highlights/industrieelektronik"},
+                    {"icon":"angewandteInformatik", "title":"Angewandte Informatik", "content":"Entwickeln von computerbasierten Lösungen", "link":"tagesschule/elektrotechnik/highlights/angewandte-informatik"}
                 ]
             },
             cards: [
@@ -29,7 +29,7 @@ export default declareComponent("ausmacht-section-elektrotechnik", class extends
                     heading: "Lernbüro",
                     note: "Unterrichtssystem",
                     thumbnail: "/res/img/student_2.jpg",
-                    href: "/",
+                    href: "tagesschule/elektrotechnik/highlights/lernbüro-elektrotechnik",
                     contentTitle: "Lernen im Aufbruch",
                     content: "Das Lernbüro ermöglicht den SchülerInnen ihren Stundenplan selbst zu organisieren. Dabei lernen sie Eigenverantwortung und Teamarbeit."
                 },
@@ -37,7 +37,7 @@ export default declareComponent("ausmacht-section-elektrotechnik", class extends
                     heading: "Werkstatt",
                     note: "PRAXISUNTERRICHT",
                     thumbnail: "/res/img/werkstaetteHET.png",
-                    href: "/",
+                    href: "tagesschule/elektrotechnik/highlights/werkstätte-elektrotechnik",
                     contentTitle: "Praxisbasierte Erfahrungen",
                     content: "In unseren Werkstätten erlernen SchülerInnen den Umgang mit technischen Gerätschaften und setzen das gelernte Theoriewissen selbst um."
                 }

2574ede393e515522cc76176124ceafeb46b6e54
Georg Felber <gfelber@student.tgm.ac.at>
2020-12-22 23:29:57 +0100

    Thumbnail: improved video transition

--- app/_component/_themeAble/_text/_thumbnail/thumbnail.ts
+++ app/_component/_themeAble/_text/_thumbnail/thumbnail.ts
@@ -77,10 +77,9 @@ export default class Thumbnail extends Text {
   private videohref:string;
   private startIframe(){
     this.q("iframe").setAttribute("src", "https://www.youtube.com/embed/" +this.videohref.split("/watch?v=").splice(-1).pop() + "?autoplay=1&controls=0")
-    this.textBlob.css({display:"none"})
     let textbox = this.q("text-box")
     let shader = this.q("background-shader");
-    [textbox,shader].forEach((elmn) => elmn.anim({opacity:0},1000));
+    [textbox].forEach((elmn) => elmn.anim({opacity:0},1000));
     setTimeout(() => [textbox,shader].forEach((elmn) => elmn.css({display:"none"})), 1000);
   }
 

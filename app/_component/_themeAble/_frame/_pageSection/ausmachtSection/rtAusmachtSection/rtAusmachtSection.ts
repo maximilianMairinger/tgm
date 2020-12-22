99468e4392f75e557288ad12d6668b34baa3ea6e
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-12-22 22:32:58 +0100

    ausmacht baselinkified

--- app/_component/_themeAble/_frame/_pageSection/ausmachtSection/rtAusmachtSection/rtAusmachtSection.ts
+++ app/_component/_themeAble/_frame/_pageSection/ausmachtSection/rtAusmachtSection/rtAusmachtSection.ts
@@ -19,64 +19,90 @@ export default declareComponent("ausmacht-section-rt", class extends AusmachtSec
     //     c-unterricht-systeme-card(heading="Werkstätte" headingBackground="#C1DBCB" note="Unsere"  thumbnail='/res/img/werk.jpg' href="/" contenttitle="Werkstätte" content='Unser Unterricht in der Werkstätte ermöglicht es den SchülerInnen auch im fachpraktischen Bereich Erfahrung zu sammeln.')
 
 
-    constructor() {
+    constructor(baseLink: string) {
         super({
             heading: {
-                heading: "Was uns Ausmacht",
+                heading: "Was uns ausmacht",
                 subheading: "als Elektrotechnik"
             },
             selection: {
-                heading: "Vertiefungen",
+                heading: "Gegenstände",
                 note: "Fachspezifische",
                 selection: [
-                    {"icon":"energysysteme", "title":"Satellitentechnik", "content":"Satellitendesign und Konstruktion", "link":"/"},
-                    {"icon":"automatisierungstechnik", "title":"Raketentechnik", "content":"Raketenentwicklung, Abschussvorbereitung", "link":"/"},
-                    {"icon":"antriebstechnik", "title":"Raumfahrtrobotik", "content":"Weltraumfähige Roboter mit autonomen Anwendungen", "link":"/"},
-                    {"icon":"industrieelektronik", "title":"Raumfahrtrobotik", "content":"Weltraumfähige Roboter mit autonomen Anwendungen", "link":"/"},
-                    {"icon":"angewandteInformatik", "title":"Raumfahrtbiologie", "content":"Biologische Untersuchungen in der Schwerelosigkeit", "link":"/"}
+                    {
+                        icon: "energysysteme", 
+                        title: "Energiesysteme", 
+                        content: "Energiegewinnung und -speicherung", 
+                        link: baseLink + "energiesysteme"
+                    },
+                    {
+                        icon: "automatisierungstechnik", 
+                        title: "Automatisierungstechnik", 
+                        content: "Automatisieren in elektrotechnischen Anwendungen", 
+                        link: baseLink + "automatisierungstechnik"
+                    },
+                    {
+                        icon: "antriebstechnik", 
+                        title: "Antriebstechnik", 
+                        content: "Bewegung und Elektrotechnik", 
+                        link: baseLink + "antriebstechnik"
+                    },
+                    {
+                        icon: "industrieelektronik", 
+                        title: "Industrieelektronik", 
+                        content: "Elektrotechnik in der Industrie", 
+                        link: baseLink + "industrieelektronik"
+                    },
+                    {
+                        icon: "angewandteInformatik", 
+                        title: "Angewandte Informatik", 
+                        content: "Entwickeln von computerbasierten Lösungen", 
+                        link: baseLink + "angewandte-informatik"
+                    }
                 ],
-                stundentafel: "tagesschule/biomedizin/highlights/stundentafel-rt"
+                stundentafel: "tagesschule/biomedizin/highlights/stundentafel-el"
             },
             cards: [
                 {
-                    heading: "Labor",
+                    heading: "Lernbüro",
                     note: "Unterrichtssystem",
-                    thumbnail: "/res/img/unterrichtSysteme_1.jpg",
-                    href: "/",
-                    contentTitle: "Im Aufbruch",
-                    content: "Das Labor ermöglicht den SchülerInnen, ihren Stundenplan selbst zu organisieren. Dabei lernen sie Eigenverantwortung und Teamarbeit."
+                    thumbnail: "/res/img/student_2.jpg",
+                    href: "tagesschule/elektrotechnik/highlights/lernbüro-elektrotechnik",
+                    contentTitle: "Lernen im Aufbruch",
+                    content: "Das Lernbüro ermöglicht den SchülerInnen ihren Stundenplan selbst zu organisieren. Dabei lernen sie Eigenverantwortung und Teamarbeit."
                 },
                 {
-                    heading: "Werkstätte",
-                    note: "Unsere",
-                    thumbnail: "/res/img/werk.jpg",
-                    href: "/",
-                    contentTitle: "Werkstätte",
-                    content: "Unser Unterricht in der Werkstätte ermöglicht es den SchülerInnen auch im fachpraktischen Bereich Erfahrung zu sammeln."
+                    heading: "Werkstatt",
+                    note: "PRAXISUNTERRICHT",
+                    thumbnail: "/res/img/werkstaetteHET.png",
+                    href: "tagesschule/elektrotechnik/highlights/werkstätte-elektrotechnik",
+                    contentTitle: "Praxisbasierte Erfahrungen",
+                    content: "In unseren Werkstätten erlernen SchülerInnen den Umgang mit technischen Gerätschaften und setzen das gelernte Theoriewissen selbst um."
                 }
             ],
             vertiefung: {
                 text: {
                     note: "die",
                     heading: "Vertiefungen",
-                    subheading: "der Raumfahrt",
+                    subheading: "der Elektrotechnik",
                     hsize: {max:60, min:40},
-                    content: "Die Abteilung Raumfahrt bietet einen besonders umfassende technische Ausbildung. Grundsätzlich beschäftigt sie sich mit allen Bereichen"
+                    content: "Die Abteilung Elektrotechnik bietet einen besonders umfassende technische Ausbildung. Grundsätzlich beschäftigt sie sich mit allen Bereichen"
                 },
                 cards: [
                     {
-                        heading:"Erneuerbare Energien",
-                        icon:"erneuerbareEnergien",
-                        link:"",
-                        content:"Die Umwandlung von Strahlungsenergie der Sonne in elektrischen Strom (Photovoltaik) bzw. in Wärme (Solarthermie) oder der Aufbau von Windkraftanlagen – das sind nur einige der nachhaltigen Technologien aus dem Bereich der Erneuerbaren Energien."
+                        heading:"Automatisierung",
+                        icon:"automatisierung",
+                        link:"tagesschule/elektrotechnik/highlights/automatisierung",
+                        content:"Das reibungslose Zusammenspiel aller Einrichtungen in einer Fabrik, der U-Bahnbetrieb, der immer vorhandene Strom in unseren Steckdosen – das alles ist ohne Automatisierungstechnik unmöglich."
                     },
                     {
                         heading:"Erneuerbare Energien",
                         icon:"erneuerbareEnergien",
-                        link:"",
+                        link:"tagesschule/elektrotechnik/highlights/erneuerbare-energien",
                         content:"Die Umwandlung von Strahlungsenergie der Sonne in elektrischen Strom (Photovoltaik) bzw. in Wärme (Solarthermie) oder der Aufbau von Windkraftanlagen – das sind nur einige der nachhaltigen Technologien aus dem Bereich der Erneuerbaren Energien."
                     }
                 ]
+                
             }
         })
     }

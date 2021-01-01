0b7b88139e347b889ebcd200776781a5c7d2ab34
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-01 19:37:21 +0100

    it icons

--- app/_component/_themeAble/_icon/icon.ts
+++ app/_component/_themeAble/_icon/icon.ts
@@ -56,16 +56,26 @@ export const iconIndex = {
     laboratorium: () => import("./fachIcon/electricGear/electricGear"),
     betriebstechnik: () => import("./fachIcon/flag/flag"),
     informatik: () => import("./fachIcon/smartCode/smartCode"),
+
+    // HIT
+    softwareentwicklung: () => import("./fachIcon/terminal/terminal"),
+    informationstechnischeProjekte: () => import("./fachIcon/workflow/workflow"),
+    netzwerktechnik: () => import("./fachIcon/graphNetwork/graphNetwork"),
+    informationssysteme: () => import("./fachIcon/cloudDataBase/cloudDataBase"),
+
   },
   vertiefung: {
     // HET
     automatisierung: () => import("./vertiefung/automationGear/automationGear"),
     erneuerbareEnergien: () => import("./vertiefung/windMills/windMills"),
 
-    // WI
+    // HWI
     maschinenbau: () => import("./vertiefung/maschinenbau/maschinenbau"),
     logistik: () => import("./vertiefung/logistik/logistik"),
     betriebsinformatik: () => import("./vertiefung/betriebsinformatik/betriebsinformatik"),
 
+    // HIT
+    medientechnik: () => import("./vertiefung/mediaCode/mediaCode"),
+    systemtechnik: () => import("./vertiefung/cloudNetwork/cloudNetwork"),
   }
 }

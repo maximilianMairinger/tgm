86e779cb3e8000ba4d577aab96c05614c1080661
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-08 01:34:14 +0100

    mbLanding2

--- app/_component/_themeAble/_frame/_page/_sectionedPage/_lazySectionedPage/mbPage/mbPage.ts
+++ app/_component/_themeAble/_frame/_page/_sectionedPage/_lazySectionedPage/mbPage/mbPage.ts
@@ -23,7 +23,7 @@ export default class MbPage extends LazySectionedPage {
           e.heading("Maschinenbau")
           e.subheading("der Tagesschule")
           e.note("abteilung");
-          e.background("mbLanding.png")
+          e.background("mbLanding2.png")
           e.videolink("https://www.youtube.com/watch?v=XIB-3K20Vaw")
           
           return new WrapperSection(e, "dark") as any

01a7836bd45ed9a79685104eb0fb6402e903fbeb
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-08 13:08:15 +0100

    vid title

--- app/_component/_themeAble/_frame/_page/_sectionedPage/_lazySectionedPage/ktPage/ktPage.ts
+++ app/_component/_themeAble/_frame/_page/_sectionedPage/_lazySectionedPage/ktPage/ktPage.ts
@@ -23,7 +23,7 @@ export default class KunststoffPage extends LazySectionedPage {
           e.heading("Kunststofftechnik")
           e.subheading("der Tagesschule")
           e.note("abteilung");
-          e.videolink("https://www.youtube.com/watch?v=7CPDqPgWjxM")
+          e.videolink("https://www.youtube.com/watch?v=7CPDqPgWjxM", "Einblick in die HKT")
           
           e.background("ktLanding.png")
           

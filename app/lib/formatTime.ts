436b06967006be99c3c286ee610572d197e8804a
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-10 19:05:24 +0100

    format time properly implemented

--- app/lib/formatTime.ts
+++ app/lib/formatTime.ts
@@ -1,14 +1,24 @@
 type FormatOptions = {weekday?: "long" | "short", year?: "numeric", month?: "2-digit" | "long" | "short" | "numeric", day?: "2-digit" | "numeric"}
 
-// TODO
-export function constructFormat(local = "de-AT", timeZone = "Europe/Vienna", defaultFormat: FormatOptions = {day: "2-digit", month: "2-digit", year: "numeric"}) {
+
+// Locals documentation https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl#Locale_identification_and_negotiation
+
+
+
+export function parseDateToLocal(dateString: string) {
+  const [ date, month, year ] = dateString.split(".")
+  return new Date(+year, +month - 1, +date)
+}
+
+export function constructFormat(local: string, timeZone = "Europe/Vienna", defaultFormat: FormatOptions = {day: "2-digit", month: "2-digit", year: "numeric"}) {
   return {
     formatDate(date: string | Date, format: FormatOptions = {}) {
-      return new Date(date).toLocaleDateString(local, { ...defaultFormat, ...format, timeZone })
+      return (date instanceof Date ? date : parseDateToLocal(date)).toLocaleDateString(local, { ...defaultFormat, ...format, timeZone })
     }
   }
 }
 
-export const AT = constructFormat()
-export const local = constructFormat()
+
+export const AT = constructFormat("de-AT")
+export const local = AT
 export default local
